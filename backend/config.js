7cde910e340d4b42579cc98cafb81111dbeee1cf
firas bentaleb <firasbentaleb@hotmail.com>
2024-02-10 18:27:07 +0100

    MongoDB connected

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDBURL = "mongodb+srv://firasbentalebdev:Cyber2018.@booking-db.hkh7whz.mongodb.net/?retryWrites=true&w=majority"
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT,mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+
 const app = express();
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+
+//setting the http route
+app.get('/',(req,res)=>{
+console.log(req)
+return res.status(234).send("App is running")
 });
+
+mongoose.connect(mongoDBURL).then(() => {
+    app.listen(PORT,()=>{
+        console.log(`Server running on port ${PORT}`);
+    });
+    
+    console.log("app Connected to database")
+}).catch((err)=> {
+    console.log(err)
+})
